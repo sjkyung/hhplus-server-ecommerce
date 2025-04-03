a73f1762bb40078ae375ca73505c05d8a39316a2
sjkyomg <sjkyomg@naver.com>
2025-04-04 04:14:00 +0900

    Feat: 포인트 서비스 Mock API, Swagger 작성

---
+++ src/main/kotlin/kr/hhplus/be/server/controller/point/request/ChargePointRequest.kt
@@ -0,0 +1,6 @@
+package kr.hhplus.be.server.controller.point.request
+
+data class ChargePointRequest(
+    val amount: Long,
+) {
+}
---
+++ src/main/kotlin/kr/hhplus/be/server/controller/point/response/PointFindResponse.kt
@@ -0,0 +1,6 @@
+package kr.hhplus.be.server.controller.point.response
+
+data class PointFindResponse(
+    val userId: Long,
+    val point: Long
+)
