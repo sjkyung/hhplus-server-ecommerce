8ee2e6da7697bf5307a58aef1de2fcc3f62acc69
sjkyomg <sjkyomg@naver.com>
2025-04-18 04:47:58 +0900

    Feat: 포인트 통합 테스트

--- src/main/kotlin/kr/hhplus/be/server/infrastructure/point/UserPointRepositoryImpl.kt
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/point/UserPointRepositoryImpl.kt
@@ -3,16 +3,25 @@ package kr.hhplus.be.server.infrastructure.point
 import kr.hhplus.be.server.domain.point.UserPoint
 import kr.hhplus.be.server.domain.point.UserPointRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
-class UserPointRepositoryImpl(): UserPointRepository {
+class UserPointRepositoryImpl(
+    private val jpaUserPointRepository: JpaUserPointRepository
+): UserPointRepository {
 
     override fun findByUserId(userId: Long): UserPoint {
-        TODO("Not yet implemented")
+        return jpaUserPointRepository.findById(userId).orElseThrow().let {
+            UserPointConverter.toDomain(it)
+        }
     }
 
-    override fun save(point: UserPoint): UserPoint {
-        TODO("Not yet implemented")
+    @Transactional
+    override fun save(userPoint: UserPoint): UserPoint {
+        val userPointEntity = jpaUserPointRepository.save(
+            UserPointConverter.toEntity(userPoint)
+        )
+        return UserPointConverter.toDomain(userPointEntity)
     }
 
 }
