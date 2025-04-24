cc576f84a9313ace3dd34e68235967b39febc565
sjkyomg <sjkyomg@naver.com>
2025-04-25 06:43:15 +0900

    Feat: 쿠폰 비관적 락 적용

--- src/main/kotlin/kr/hhplus/be/server/infrastructure/coupon/JpaCouponRepository.kt
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/coupon/JpaCouponRepository.kt
@@ -1,6 +1,10 @@
 package kr.hhplus.be.server.infrastructure.coupon
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 
 interface JpaCouponRepository: JpaRepository<CouponEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    fun findWithLockById(id: Long): CouponEntity
 }
