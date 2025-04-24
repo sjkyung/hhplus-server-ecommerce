eb19733fc9f5b68827bf2a0b974b796fe7f06a40
sjkyomg <sjkyomg@naver.com>
2025-04-25 06:50:04 +0900

    Feat: E2E TEST Base 추가

---
+++ src/test/java/kr/hhplus/be/server/support/E2ETestBase.kt
@@ -0,0 +1,61 @@
+package kr.hhplus.be.server.support
+
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@ActiveProfiles("test")
+abstract class E2ETestBase {
+
+    @LocalServerPort
+    var port: Int = 8080
+
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    @BeforeEach
+    fun cleanDatabase() {
+
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0")
+
+        val tableNames = jdbcTemplate.queryForList(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = (SELECT DATABASE())",
+            String::class.java
+        )
+
+        tableNames.forEach { tableName ->
+            jdbcTemplate.execute("TRUNCATE TABLE $tableName")
+        }
+
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1")
+    }
+
+    companion object {
+        @Container
+        val mysql = MySQLContainer(DockerImageName.parse("mysql:8.0")).apply {
+            withDatabaseName("hhplus")
+            withUsername("application")
+            withPassword("application")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun overrideProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", mysql::getJdbcUrl)
+            registry.add("spring.datasource.username", mysql::getUsername)
+            registry.add("spring.datasource.password", mysql::getPassword)
+        }
+    }
+}
