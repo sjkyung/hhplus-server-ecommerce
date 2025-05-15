d995fe5baf78e7210bffed3423e1bc17501c2572
sjkyomg <sjkyomg@naver.com>
2025-05-16 04:55:23 +0900

    Feat: refactor

--- src/main/kotlin/kr/hhplus/be/server/interfaces/product/ProductController.kt
+++ src/main/kotlin/kr/hhplus/be/server/interfaces/product/ProductController.kt
@@ -1,7 +1,9 @@
 package kr.hhplus.be.server.interfaces.product
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import kr.hhplus.be.server.application.product.ProductService
 import kr.hhplus.be.server.ApiResponse
+import kr.hhplus.be.server.application.product.ProductResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1")
 class ProductController(
-    private val productService: ProductService
+    private val productService: ProductService,
+    private val objectMapper: ObjectMapper
 ) : ProductApiSpec {
 
     @GetMapping("/products")
@@ -22,6 +25,17 @@ class ProductController(
     @GetMapping("/products/rank")
     override fun getProductRanking(): ApiResponse<List<ProductRankingResponse>> {
         val rankingProducts = productService.findRankingByProducts()
+        val response = rankingProducts.map { ProductRankingResponse.from(it) }
+        return ApiResponse.success(response)
+    }
+
+    @GetMapping("/products/rank/cache")
+    fun getProductRankingCache(): ApiResponse<List<ProductRankingResponse>> {
+        val rankingProducts = productService.findRankingByProductsCache()
+        val fixed = rankingProducts.map {
+            if (it is ProductResult) it
+            else objectMapper.convertValue(it, ProductResult::class.java)
+        }
         val response = rankingProducts.map { ProductRankingResponse.from(it) }
         return ApiResponse.success(response)
     }
